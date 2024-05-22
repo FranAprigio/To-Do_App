7a4e7e131d42fa7cdea21a8d20336aabba8fe880
Franciane Aprigio <francianefad@outlook.com>
2024-05-22 01:23:32 -0300

    [FEAT] - Updating routes

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import React from "react";
-import { StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Initial from './screens/Initial';
+import Register from './screens/Register';
+import Login from './screens/Login';
+import Home from './screens/Home';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Dashboard from "./src/pages/Dashboard/Dashboard"
-import Login from "./src/pages/Login/Login"
-import Registration from "./src/pages/Registration/Registration"
-import Home from "./src/pages/HomeScreen/Home"
-import Lista from "./src/components/List/Lista"
-import MapScreen from "./src/components/Location/MapScreen";
- 
-const Stack = createNativeStackNavigator(); 
+import { NavigationContainer } from '@react-navigation/native';
 
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
-    <NavigationContainer styles={styles.container}>
-      <Stack.Navigator initialRouteName="Dashboard">
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Registration" component={Registration} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Lista" component={Lista} />
-        <Stack.Screen name="Map" component={MapScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+	<NavigationContainer>
+		<Stack.Navigator initialRouteName='Initial'>
+      <Stack.Screen name='Initial' component={Initial} />
+			<Stack.Screen name='Login' component={Login} />
+			<Stack.Screen name='Register' component={Register} />
+			<Stack.Screen name='Home' component={Home} />
+		</Stack.Navigator>
+	</NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E6E6E6',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
